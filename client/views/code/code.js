5589a4fbc70c8afd1c02801e9a35460067bf7362
Parthiban Loganathan <pl2487@columbia.edu>
2014-09-10 10:50:12 -0400

    Start adding code page

---
+++ client/views/code/code.js
@@ -0,0 +1,12 @@
+var projectsData = [
+{
+  name: "E6B+",
+  description: "A flight computer app for Android",
+  url: "https://play.google.com/store/apps/details?id=com.e6bactivities&hl=en",
+  image: "img/coursera.png"
+}
+];
+
+Template.code.helpers({
+  projects: projectsData
+});
