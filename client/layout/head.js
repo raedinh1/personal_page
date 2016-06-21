f347a29a15478f43eaf00d028557a8b7aa117b93
Parthiban Loganathan <parthibanloganathan@users.noreply.github.com>
2016-06-21 12:40:42 -0400

    remove isclient check

--- client/layout/head.js
+++ client/layout/head.js
@@ -1,13 +1,11 @@
 Meteor.startup(function() {
-  if (Meteor.isClient) {
-    return SEO.config({
-      title: 'Parthiban Loganathan',
-      meta: {
-        'description': "Parthi's node on the world wide web."
-      },
-      og: {
-        'title': 'Parthiban Loganathan'
-      }
-    });
-  }
+  return SEO.config({
+    title: 'Parthiban Loganathan',
+    meta: {
+      'description': "Parthi's node on the world wide web."
+    },
+    og: {
+      'title': 'Parthiban Loganathan'
+    }
+  });
 });
