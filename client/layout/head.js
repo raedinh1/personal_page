5c2f409d9bb7bbce63f71429a28d6b1b4fb39b57
Parthiban Loganathan <pl2487@columbia.edu>
2016-07-03 20:54:07 -0400

    update title in js

--- client/layout/head.js
+++ client/layout/head.js
@@ -2,7 +2,7 @@ Meteor.startup(function() {
   return SEO.config({
     title: 'Parthiban Loganathan',
     meta: {
-      'description': "Parthi's node on the world wide web."
+      'description': "Parthiban Loganathan. Welcome to Parthi's node on the world wide web. Currently working on product at Google. Studied Computer Science at Columbia."
     },
     og: {
       'title': 'Parthiban Loganathan'
